e981391f9d20f0eb1b55dc7341b73223af0281dc
elKevgodoy <k.godoy@duocuc.cl>
2023-06-08 23:53:07 -0400

    Pages Registro y Reserva Creados

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('registro', views.registro, name='registro'),
+    path('reserva', views.reserva, name='reserva')
 ]
--- app/views.py
+++ app/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request): 
     return render(request, 'home.html')
+
+def registro(request): 
+    return render(request, 'registro.html')
+
+def reserva(request): 
+    return render(request, 'reserva.html')
